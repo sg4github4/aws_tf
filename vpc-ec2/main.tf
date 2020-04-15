edea2a78f271229242c0c6cb26117f82ad989755
Shravan Gasikanti <sxgasik@lrem-c02zl0mp.na.global.prv>
2020-04-14 21:19:35 -0400

    added S3 bucket policy for CUR, Creating CUR report

--- vpc-ec2/main.tf
+++ vpc-ec2/main.tf
@@ -42,6 +42,51 @@ bucket = "sgasiks3bucket1"
 acl    = "private"
 }
 
+#Create S3 bucket Policy
+resource "aws_s3_bucket_policy" "sgasiks3bucket1" {
+  bucket = "${aws_s3_bucket.sgasiks3bucket1.id}"
+
+  policy = <<POLICY
+{
+ "Version": "2012-10-17",
+ "Statement": [
+ {
+ "Effect": "Allow",
+ "Principal": {
+ "Service": "billingreports.amazonaws.com"
+ },
+ "Action": [
+ "s3:GetBucketAcl",
+ "s3:GetBucketPolicy"
+ ],
+ "Resource": "arn:aws:s3:::sgasiks3bucket1"
+ },
+ {
+ "Effect": "Allow",
+ "Principal": {
+ "Service": "billingreports.amazonaws.com"
+ },
+ "Action": "s3:PutObject",
+ "Resource": "arn:aws:s3:::sgasiks3bucket1/*"
+ }
+ ]
+}
+ POLICY
+}
+
+#Create CUR (Cost and Usage Report)
+resource "aws_cur_report_definition" "sgasik_cur_report_definition" {
+  report_name                = "sgasik-cur-report-definition"
+  time_unit                  = "HOURLY"
+  format                     = "textORcsv"
+  compression                = "GZIP"
+  additional_schema_elements = ["RESOURCES"]
+  s3_bucket                  = "sgasiks3bucket1"
+  s3_region                  = "us-east-1"
+  s3_prefix                  = "/sgasik/jan2mar"
+  additional_artifacts       = ["REDSHIFT", "QUICKSIGHT"]
+}
+
 #Create Athena Database
 resource "aws_athena_database" "sgasiks3bucket1" {
   name = "sgasik_athenadb"
@@ -53,7 +98,7 @@ resource "aws_athena_workgroup" "sgasik_AthenaWG1" {
   name = "sgasik_AthenaWG1"
 
   configuration {
-    enforce_workgroup_configuration    = true
+    enforce_workgroup_configuration = true
     publish_cloudwatch_metrics_enabled = true
 
     result_configuration {
